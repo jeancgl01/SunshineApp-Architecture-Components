7446a5ac23b22942ae7abfc873d29c584038f346
juancarlosgallegos <juan.gallegos@bachoco.net>
2018-06-12 13:30:56 -0500

    metodo fetchWeather() compatible con Java 1.7

--- app/src/main/java/com/mtw/juancarlos/sunshineapp/data/network/WeatherNetworkDataSource.java
+++ app/src/main/java/com/mtw/juancarlos/sunshineapp/data/network/WeatherNetworkDataSource.java
@@ -138,41 +138,44 @@ public class WeatherNetworkDataSource {
      */
     void fetchWeather() {
         Log.d(LOG_TAG, "Fetch weather started");
-     /*   mExecutors.networkIO().execute(() -> {
-            try {
+        mExecutors.networkIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-                // The getUrl method will return the URL that we need to get the forecast JSON for the
-                // weather. It will decide whether to create a URL based off of the latitude and
-                // longitude or off of a simple location as a String.
+                    // The getUrl method will return the URL that we need to get the forecast JSON for the
+                    // weather. It will decide whether to create a URL based off of the latitude and
+                    // longitude or off of a simple location as a String.
 
-                URL weatherRequestUrl = NetworkUtils.getUrl();
+                    URL weatherRequestUrl = NetworkUtils.getUrl();
 
-                // Use the URL to retrieve the JSON
-                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                    // Use the URL to retrieve the JSON
+                    String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
 
-                // Parse the JSON into a list of weather forecasts
-                WeatherResponse response = new OpenWeatherJsonParser().parse(jsonWeatherResponse);
-                Log.d(LOG_TAG, "JSON Parsing finished");
+                    // Parse the JSON into a list of weather forecasts
+                    WeatherResponse response = new OpenWeatherJsonParser().parse(jsonWeatherResponse);
+                    Log.d(LOG_TAG, "JSON Parsing finished");
 
 
-                // As long as there are weather forecasts, update the LiveData storing the most recent
-                // weather forecasts. This will trigger observers of that LiveData, such as the
-                // SunshineRepository.
-                if (response != null && response.getWeatherForecast().length != 0) {
-                    Log.d(LOG_TAG, "JSON not null and has " + response.getWeatherForecast().length
-                            + " values");
-                    Log.d(LOG_TAG, String.format("First value is %1.0f and %1.0f",
-                            response.getWeatherForecast()[0].getMin(),
-                            response.getWeatherForecast()[0].getMax()));
+                    // As long as there are weather forecasts, update the LiveData storing the most recent
+                    // weather forecasts. This will trigger observers of that LiveData, such as the
+                    // SunshineRepository.
+                    if (response != null && response.getWeatherForecast().length != 0) {
+                        Log.d(LOG_TAG, "JSON not null and has " + response.getWeatherForecast().length
+                                + " values");
+                        Log.d(LOG_TAG, String.format("First value is %1.0f and %1.0f",
+                                response.getWeatherForecast()[0].getMin(),
+                                response.getWeatherForecast()[0].getMax()));
 
-                    // TODO Finish this method when instructed.
-                    // Will eventually do something with the downloaded data
+                        // TODO Finish this method when instructed.
+                        // Will eventually do something with the downloaded data
+                    }
+                } catch (Exception e) {
+                    // Server probably invalid
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                // Server probably invalid
-                e.printStackTrace();
             }
-        });*/
+        });
     }
 
 }
